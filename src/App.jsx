2754741b0306b77969786f4755395415d35e9712
Vikky1123 <122759969+Vikky1123@users.noreply.github.com>
2025-04-27 00:47:39 +0000

    Restored to '1d1ee4a1db0494ec70bf9ac72c8ffaadf786ff3c'
    Replit-Restored-To: 1d1ee4a1db0494ec70bf9ac72c8ffaadf786ff3c

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ErrorBoundary from './components/ErrorBoundary';
-import Banner from './components/MainSection/HeroBanner';
+import ErrorBoundary from './components/ErrorBoundary.jsx';
+import Banner from './components/Banner';
 import MainSection from './components/MainSection/MainSection';
 
 function App() {
